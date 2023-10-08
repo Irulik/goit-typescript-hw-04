b164975667f3cde052f8f42f070470f0617056a7
Irulik <123405063+Irulik@users.noreply.github.com>
2023-10-08 17:43:09 +0200

    Update 4.tsx

--- src/homework/4.tsx
+++ src/homework/4.tsx
@@ -4,20 +4,32 @@ import noop from "lodash/noop";
 type MenuIds = "first" | "second" | "last";
 type Menu = { id: MenuIds; title: string };
 
-// Додати тип Menu Selected
+// Оголошую тип SelectedMenu
+type SelectedMenu = {
+  id?: MenuIds;
+};
+
+// Оголошую тип MenuSelected
+type MenuSelected = {
+  selectedMenu: SelectedMenu;
+};
+
+// Оголошую тип MenuAction
+type MenuAction = {
+  onSelectedMenu: (selectedMenu: SelectedMenu) => void;
+};
 
 const MenuSelectedContext = createContext<MenuSelected>({
   selectedMenu: {},
 });
 
-// Додайте тип MenuAction
-
 const MenuActionContext = createContext<MenuAction>({
   onSelectedMenu: noop,
 });
 
+
 type PropsProvider = {
-  children; // Додати тип для children
+  children: ReactNode;
 };
 
 function MenuProvider({ children }: PropsProvider) {
@@ -48,7 +60,7 @@ function MenuProvider({ children }: PropsProvider) {
 }
 
 type PropsMenu = {
-  menus; // Додайте вірний тип для меню
+  menus: Menu[];
 };
 
 function MenuComponent({ menus }: PropsMenu) {
